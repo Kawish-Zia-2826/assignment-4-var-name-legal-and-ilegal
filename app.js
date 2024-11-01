dddaed4ccc229bef8750b07f1b772341cff5a00c
Kawish Zia <kawishzia2826@gmail.com>
2024-11-01 03:00:08 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+// //---------------------------------------------------------------------JS Assignment 04(VARIABLE NAMES: LEGAL & ILLEGAL)
+// //----------------------------------------------------------------------Task No 01----------------------------------------------------------------------
+// // 1. Declare 3 variables in one statement.
+// // 2. Declare 5 legal & 5 illegal variable names.
+// // 3. Display this in your browser
+// // a) A heading stating “Rules for naming JS variables”
+// // b) Variable names can only contain ______, ______,
+// // ______ and ______.
+// // For example $my_1stVariable
+// // c) Variables must begin with a ______, ______ or
+// // _____. For example $name, _name or name
+// // d) Variable names are case _________
+// // e) Variable names should not be JS _________
+
+// //----------------------------------------------------------------------Task No 01----------------------------------------------------------------------
+
+//  let abc;
+//  const abcd = 1;
+//  var abcde;
+
+
+
+// //----------------------------------------------------------------------Task No 02----------------------------------------------------------------------
+
+// var Geeks;
+// var Geeks_for_Geeks;
+// var $Geeks;
+// var _Geeks;
+// var Geeks123;
+// //var Geeks-for-Geeks;  // Hyphens not allowed
+// //var 123Geeks;  // Can not start with numbers
+// //var Geeks for Geeks;  // Spaces not allowed
+// //----------------------------------------------------------------------Task No 03----------------------------------------------------------------------
+// // 3. Display this in your browser
+// document.write("A heading stating “Rules for naming JS variables” b) Variable names can only contain number, $,underscore For example $my_1stVariable c) Variables must begin with a _letter,$, underscore. For example  $name, _name or name $name, _name or name d) Variable names are case __sensetivee) Variable names should not be JS keyword");
